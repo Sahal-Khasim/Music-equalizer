64e44f060c450f4b6002252b0b95c8f9294a17c2
shafafmuhammad6@gmail.com <shafafmuhammad6@gmail.com>
2021-05-21 22:52:01 +0530

    template added

--- peak/peak/settings.py
+++ peak/peak/settings.py
@@ -75,16 +75,12 @@ WSGI_APPLICATION = 'peak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'equalizerdb',  
-        'USER':'root',  
-        'PASSWORD':'',  
-        'HOST':'127.0.0.1',  
-        'PORT':'3306'  
-    }  
-} 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
