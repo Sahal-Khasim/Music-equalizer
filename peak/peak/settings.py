a910d5de801008cfb10969338754dbe18f26c41c
shafafmuhammad6@gmail.com <shafafmuhammad6@gmail.com>
2021-05-22 19:06:43 +0530

    register and login added

--- peak/peak/settings.py
+++ peak/peak/settings.py
@@ -75,15 +75,11 @@ WSGI_APPLICATION = 'peak.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'equalizerdb',  
-        'USER':'root',  
-        'PASSWORD':'',  
-        'HOST':'127.0.0.1',  
-        'PORT':'3306'  
-    }  
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
